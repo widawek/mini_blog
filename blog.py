b9db6f3ddcc3d841e66cceb773e0f13fb675a57d
widawek <kolczywiewicz@gmail.com>
2024-01-24 22:29:33 +0100

    Update blog.py and and new template homepage.py

--- blog.py
+++ blog.py
@@ -1,13 +1,14 @@
 from blog import app, db
-from blog.models import User, Post
+from blog import models
+
 
 @app.shell_context_processor
 def make_shell_context():
     return {
         "db": db,
-        "User": User,
-        "Post": Post
+        "Entry": models.Entry
     }
+
 
 if __name__ == "__main__":
     app.run()
--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from blog import app
+from blog.models import Entry
 
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    all_posts = Entry.query.filter_by(is_published=True)\
+        .order_by(Entry.pub_date.desc())
+
+    return render_template("homepage.html", all_posts=all_posts)
