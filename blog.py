610640bbacae8c5476669753f26f0221a65a3049
widawek <kolczywiewicz@gmail.com>
2024-01-24 22:12:37 +0100

    Create database and and files .flaskenv and requirements.txt

---
+++ blog.py
@@ -0,0 +1,13 @@
+from blog import app, db
+from blog.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "User": User,
+        "Post": Post
+    }
+
+if __name__ == "__main__":
+    app.run()
--- blog/__init__.py
+++ blog/__init__.py
@@ -8,4 +8,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Entry": models.Entry
+    }
+
 from blog import routes, models
